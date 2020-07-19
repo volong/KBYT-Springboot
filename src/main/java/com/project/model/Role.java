f77af8c29205164b72b9caa3a15a37c48d35fc72
Võ Long <maccualongne@SEALSs-MacBook-Pro.local>
2020-07-19 11:11:05 +0700

    lên thớt

--- src/main/java/com/project/model/Role.java
+++ src/main/java/com/project/model/Role.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "roles")
+@Table(name = "role")
 public class Role implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/project/model/User.java
+++ src/main/java/com/project/model/User.java
@@ -15,7 +15,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
